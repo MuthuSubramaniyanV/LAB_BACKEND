14f7c3e9dda635a3cc1cd477a5a10979d61304b6
MuthuSubramanianV <muthusubramanian01@gmail.com>
2026-06-11 01:17:32 +0530

    API integration

--- main.py
+++ main.py
@@ -22,9 +22,6 @@ genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
-app = FastAPI()
-
-
 @app.get("/")
 def home():
     return {"status": "working"}
